6771e2317ce55bd119a4f333f6369c72e2012660
evkondr <kondratiev.evg@gmail.com>
2022-05-08 18:55:30 +0300

    feat: install and configure eslint, correct syntax according to eslin rules

--- src/index.js
+++ src/index.js
@@ -8,37 +8,35 @@ const keyboardWrap = document.createElement('div');
 
 wrap.className = 'wrap';
 
-keyboardWrap.className = 'keyboard'
-
-for(let line in keyboard){
-    const raw = document.createElement('div');
-    raw.className = 'raw';
-    console.log(raw)
-    for(let key of keyboard[line]){
-        const button = document.createElement('div');
-        button.className = `button ${key.color}`;
-        button.innerHTML = key.char;
-        if(key.type) {
-            switch (key.type) {
-                case 'arrow':
-                    button.style.flexGrow = 0;
-                    break;
-                case 'spacebar':
-                    button.style.width = '50%';
-                    break;
-                case 'right_shift':
-                    button.style.flexGrow = 0;
-                    button.style.padding = 0;
-                    break;
-                default:
-                    break;
-            }   
-        }
-        raw.append(button);
+keyboardWrap.className = 'keyboard';
+Object.keys(keyboard).forEach((line) => {
+  const raw = document.createElement('div');
+  raw.className = 'raw';
+  keyboard[line].forEach((key) => {
+    const button = document.createElement('div');
+    button.className = `button ${key.color}`;
+    button.innerHTML = key.char;
+    if (key.type) {
+      switch (key.type) {
+        case 'arrow':
+          button.style.flexGrow = 0;
+          break;
+        case 'spacebar':
+          button.style.width = '50%';
+          break;
+        case 'right_shift':
+          button.style.flexGrow = 0;
+          button.style.padding = 0;
+          break;
+        default:
+          break;
+      }
     }
-    keyboardWrap.append(raw)
-}
-wrap.append(keyboardWrap)
+    raw.append(button);
+  });
+  keyboardWrap.append(raw);
+});
+wrap.append(keyboardWrap);
 
 document.title = variables.title;
 document.body.append(wrap);
