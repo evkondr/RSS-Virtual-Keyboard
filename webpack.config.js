cdcf918c1d7258726969b284de26e3f830167541
evkondr <kondratiev.evg@gmail.com>
2022-05-10 00:19:30 +0300

    feat: add build

--- webpack.config.js
+++ webpack.config.js
@@ -1,46 +1,48 @@
 const path = require('path');
-const HtmlWebpackPlugin = require("html-webpack-plugin");
-const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+
 module.exports = {
-    mode: "development",
-    entry: "./src/index.js",
-    output: {
-        path: path.resolve(__dirname + "/public"),
-        filename: "bundle.js",
-        assetModuleFilename: 'assets/[hash][ext][query]',
-        clean: true,
-    },
-    module: {
-        rules: [
-        {   
-          test: /\.(scss|css)$/,
-          use: [MiniCssExtractPlugin.loader, 'css-loader', 'resolve-url-loader',{
-              loader: 'sass-loader',
-              options: {
-                sourceMap: true
-              }}],
-        },
-        {
-            test: /\.html$/,
-            loader: "html-loader"
-        },
-        {
-            test: /\.(png|jpg|gif)$/i,
-            type: 'asset/resource'
-          }
-      ],
-    },
-    plugins: [
-        new MiniCssExtractPlugin(),
-        new HtmlWebpackPlugin({
-            filename: "./index.html",
-        })
-    ],
-    devServer: {
-        static: {
-          directory: path.join(__dirname, '/public'),
-        },
-        compress: true,
-        port: 9000,
+  mode: 'development',
+  entry: './src/index.js',
+  output: {
+    path: path.resolve(`${__dirname}/public`),
+    filename: 'bundle.js',
+    assetModuleFilename: 'assets/[hash][ext][query]',
+    clean: true,
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(scss|css)$/,
+        use: [MiniCssExtractPlugin.loader, 'css-loader', 'resolve-url-loader', {
+          loader: 'sass-loader',
+          options: {
+            sourceMap: true,
+          },
+        }],
       },
-  };
+      {
+        test: /\.html$/,
+        loader: 'html-loader',
+      },
+      {
+        test: /\.(png|jpg|gif)$/i,
+        type: 'asset/resource',
+      },
+    ],
+  },
+  plugins: [
+    new MiniCssExtractPlugin(),
+    new HtmlWebpackPlugin({
+      filename: './index.html',
+    }),
+  ],
+  devServer: {
+    static: {
+      directory: path.join(__dirname, '/public'),
+    },
+    compress: true,
+    port: 9000,
+  },
+};
